e9f1751cee7c742a525e5f774d49760d77d33fc8
Mickeypy <mickey7890tair@gmail.com>
2025-02-27 18:35:37 +0700

    Modified Readme.md, add arduino code

---
+++ arduinoDrP/arduinoDrP.ino
@@ -0,0 +1,17 @@
+int ledPin = 3; // ใช้ LED บนบอร์ด
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();  // อ่านค่าจาก Serial
+    if (command == '1') {
+      digitalWrite(ledPin, HIGH);  // เปิดไฟ LED ถ้าตรวจพบวัตถุ
+      delay(500);
+      digitalWrite(ledPin, LOW);   // ปิดไฟ LED
+    }
+  }
+}
